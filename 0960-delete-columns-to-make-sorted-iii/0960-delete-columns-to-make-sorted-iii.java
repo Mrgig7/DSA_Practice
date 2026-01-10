bc3a2e69c00b04f4dfadc55216866e07d4a1447e
Nitesh J <jnitesh1463@gmail.com>
2026-01-10 15:52:14 +0530

    Time: 6 ms (99.46%), Space: 46 MB (5.02%) - LeetHub

--- 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
@@ -1,18 +1,23 @@
 class Solution {
     public int minDeletionSize(String[] st) {
         int n=st.length,m=st[0].length();
+        char[][]ch=new char[n][];
+        for (int i=0;i<n;i++)ch[i]=st[i].toCharArray();
+        boolean[][]ok=new boolean[m][m];
+        for (int i=0;i<m;i++){
+            for (int j=i+1;j<m;j++){
+                boolean gd=true;
+                for (int r=0;r<n;r++)if (ch[r][i]>ch[r][j]){gd=false;break;}
+                ok[i][j]=gd;
+            }
+        }
         int[] dp=new int[m];
         int bst=0;
         for (int j=0;j<m;j++){
-            dp[j]=1;
-            for (int i=0;i<j;i++){
-                boolean ok=true;
-                for (int r=0;r<n;r++){
-                    if (st[r].charAt(i)>st[r].charAt(j)){ok=false;break;}
-                }
-                if (ok)dp[j]=Math.max(dp[j],dp[i]+1);
-            }
-            bst=Math.max(bst,dp[j]);
+            int mx=1;
+            for (int i=0;i<j;i++)if(ok[i][j])mx=Math.max(mx,dp[i]+1);
+            dp[j]=mx;
+            if (mx>bst)bst=mx;
         }
         return m-bst;
     }
